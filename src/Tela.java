ffa4587d8b517f3b15d054768c00d37950a265ff
m4rcelodiasBR <m4rcelodias@live.com>
2021-04-25 16:50:07 -0300

    Ajustes Linha 38

--- src/Tela.java
+++ src/Tela.java
@@ -35,7 +35,7 @@ public class Tela extends JFrame {
         getContentPane().setLayout(layout);
         
         
-        List lEsq = new List(noLinhas, true); teste;
+        List lEsq = new List(noLinhas, true);
         lEsq.add("Um");
         lEsq.add("Dois");
         lEsq.add("Três");
